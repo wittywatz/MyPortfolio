7992443443ff867f88e94330a6c8b3ca5b2f1f0f
WittyWatzz <35387099+WittyWatzz@users.noreply.github.com>
2021-03-10 15:28:27 -0500

    Video left

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,19 +13,36 @@ const Footer = () => {
         <div className="footer-body pt-2">
           <div className="footer-content" style={{ zIndex: '5' }}>
             <div className="mb-0 p-4">
-              <a href="https://github.com/wittywatz" style={linkStyle}>
+              <a
+                href="https://github.com/wittywatz"
+                style={linkStyle}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <i className="fab fa-github icon-footer"></i>
               </a>
               <a
                 href="https://www.linkedin.com/in/watson-agbramu-a37320181/"
                 style={linkStyle}
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 <i className="fab ml-4 icon-footer fa-linkedin-in"></i>
               </a>
-              <a href="https://www.instagram.com/wittywatz" style={linkStyle}>
+              <a
+                href="https://www.instagram.com/wittywatz"
+                style={linkStyle}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <i className="fab ml-4 fa-instagram icon-footer"></i>
               </a>
-              <a style={linkStyle} href="mailto:watsonagbramu@gmail.com">
+              <a
+                style={linkStyle}
+                href="mailto:watsonagbramu@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <i className="fas fa-envelope-open-text icon-footer ml-4"></i>
               </a>
 
