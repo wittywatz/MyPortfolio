c93b0dcd9531b62a083da925a6bce9c4e72494cc
wittywatz <wittywatz@gmail.com>
2023-05-29 14:28:34 -0400

    Updates

---
+++ precache-manifest.bdd47495d57ecd5d6e1beb48b41206a9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d27752dd0da4e554f24",
+    "url": "/MyPortfolio/static/css/main.6db7582c.chunk.css"
+  },
+  {
+    "revision": "7d27752dd0da4e554f24",
+    "url": "/MyPortfolio/static/js/main.1b7c50a1.chunk.js"
+  },
+  {
+    "revision": "41ef2c5e8e68d7a2a057",
+    "url": "/MyPortfolio/static/js/runtime~main.ccce04bf.js"
+  },
+  {
+    "revision": "d2f3c3bf1e6d94002a4e",
+    "url": "/MyPortfolio/static/css/2.c9f97ffc.chunk.css"
+  },
+  {
+    "revision": "d2f3c3bf1e6d94002a4e",
+    "url": "/MyPortfolio/static/js/2.6e9b111a.chunk.js"
+  },
+  {
+    "revision": "ce425f3fe6c6885402edc2b857e7a97e",
+    "url": "/MyPortfolio/static/media/Resume.ce425f3f.pdf"
+  },
+  {
+    "revision": "68c8fee63b55fbc8bad939c61b04feb2",
+    "url": "/MyPortfolio/static/media/homepage.68c8fee6.png"
+  },
+  {
+    "revision": "d22973b66efbac20eb2c3dd0998bcd7c",
+    "url": "/MyPortfolio/static/media/profile.d22973b6.jpg"
+  },
+  {
+    "revision": "4fe4044e89a14440a0e48945ebb2dd93",
+    "url": "/MyPortfolio/index.html"
+  }
+];
